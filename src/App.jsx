2afdc71f6e52de0c40d99e1b225f28f98d2c7fbe
richardscheid <rscheid@live.com>
2020-07-13 21:50:31 -0300

    refact: change primary color

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { GlobalStyle, theme } from '@involves/violin';
-import { AppContainer, PageContainer } from './style';
 import { ThemeProvider } from 'styled-components';
+import { AppContainer, PageContainer } from './style';
+import { GlobalStyle, theme, createTheme } from '@involves/violin';
 
 import Routes from './routes';
 
+const customTheme = createTheme({
+  palette: {
+    primary: theme.palette.cyan,
+  },
+});
+
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={customTheme}>
       <AppContainer>
         <GlobalStyle />
         <PageContainer>
