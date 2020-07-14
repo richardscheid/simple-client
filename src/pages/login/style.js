d0d3bf451ef9986fa59d041f4728dfba0429b288
richardscheid <rscheid@live.com>
2020-07-14 13:46:17 -0300

    fix: backgroud image

--- src/pages/login/style.js
+++ src/pages/login/style.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
+import dashboard from '../../assets/dashboard.jpg';
 
 export const LoginContainer = styled.div`
   width: 50%;
-  align-self: center;
-  background: url(dashboard.jpg) blue !important;
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+  background-image: url(${dashboard}) !important;
 `;
 
 export const LoginForm = styled.form`
