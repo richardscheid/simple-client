a8b109c0fd81d57ae87dfc522daffca40f54f0f4
richardscheid <rscheid@live.com>
2020-07-13 22:19:41 -0300

    chore: add background image

--- src/pages/login/style.js
+++ src/pages/login/style.js
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 export const LoginContainer = styled.div`
   width: 50%;
   align-self: center;
+  background: url(dashboard.jpg) blue !important;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 `;
 
 export const LoginForm = styled.form`
