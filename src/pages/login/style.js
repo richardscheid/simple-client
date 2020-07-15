6f44d2569ac98df2620615a4f6df59f6f8dec2a7
richardscheid <rscheid@live.com>
2020-07-15 16:09:43 -0300

    feat: create new login content

--- src/pages/login/style.js
+++ src/pages/login/style.js
@@ -5,15 +5,17 @@ export const LoginContainer = styled.div`
   width: 100%;
   height: 100vh;
   margin: 0 auto;
-
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
   background-image: url(${dashboard}) !important;
 `;
 
-export const LoginForm = styled.form``;
+export const LoginContent = styled.form`
+  width: 250px;
+  margin: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f0f0f5;
+`;
