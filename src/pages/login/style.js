d5f9da0cfba4fb58a98753349ad466dfebbcc011
richardscheid <rscheid@live.com>
2020-07-16 17:42:23 -0300

    refact: create login styled component

--- src/pages/login/style.js
+++ src/pages/login/style.js
@@ -1,21 +1,26 @@
 import styled from 'styled-components';
 import dashboard from '../../assets/dashboard.jpg';
 
-export const LoginContainer = styled.div`
+const LoginStyle = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const LoginContainer = styled(LoginStyle)`
   width: 100%;
   height: 100vh;
   margin: 0 auto;
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  background-image: url(${dashboard}) !important;
+  background-image: url(${dashboard});
 `;
 
-export const LoginContent = styled.form`
-  width: 250px;
+export const LoginContent = styled(LoginStyle)`
+  width: 350px;
+  height: 250px;
   margin: 30px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   background-color: #f0f0f5;
 `;
