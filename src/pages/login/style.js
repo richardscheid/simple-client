75666695cab737a74f042285ffa25cc1bf75a7ab
richardscheid <rscheid@live.com>
2020-07-17 17:41:27 -0300

    feat: create form content style

--- src/pages/login/style.js
+++ src/pages/login/style.js
@@ -10,8 +10,6 @@ const LoginStyle = styled.div`
 
 export const LoginContainer = styled(LoginStyle)`
   width: 100%;
-  height: 100vh;
-  margin: 0 auto;
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
@@ -21,7 +19,10 @@ export const LoginContainer = styled(LoginStyle)`
 export const LoginContent = styled(LoginStyle)`
   width: 350px;
   height: 250px;
-  margin: 30px;
   border-radius: 0.5rem;
   background-color: #f0f0f5;
+`;
+
+export const FormContent = styled.div`
+  width: 85%;
 `;
