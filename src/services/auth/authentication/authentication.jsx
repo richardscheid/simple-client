0357f744100552ac8881559354e949bfa02facb8
richardscheid <rscheid@live.com>
2020-08-08 18:58:39 -0300

    refact: improving code readability

--- src/services/auth/authentication/authentication.jsx
+++ src/services/auth/authentication/authentication.jsx
@@ -1,15 +1,13 @@
 import api from '../../api';
 
-class Authentication {
-  async login({ email, password }) {
-    try {
-      const result = await api.post('login', { email, password });
+const login = async ({ email = '', password = '' }) => {
+  try {
+    const result = await api.post('login', { email, password });
 
-      return result;
-    } catch (err) {
-      console.log(err);
-    }
+    return result;
+  } catch (err) {
+    console.log(err);
   }
-}
+};
 
-export default Authentication;
+export default { login };
