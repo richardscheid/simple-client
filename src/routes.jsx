90365119249490d3d3b1dfab6f2376fbe57931a5
richardscheid <rscheid@live.com>
2020-08-19 19:18:48 -0300

    chore: add routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Login from './pages/login/';
+import Login from './pages/login';
+import Dashboard from './pages/dashboard';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' exact component={Login} />
+        <Route path='/' exact>
+          <Login />
+        </Route>
+
+        <Route path='/dashboard'>
+          <Dashboard />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
