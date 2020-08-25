0183fce25c951f690dddf7bf10f7c4fe36111820
richardscheid <rscheid@live.com>
2020-08-25 14:47:32 -0300

    chore: create app theme

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+import { red } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    error: {
+      main: red.A400,
+    }
+  },
+  typography: {
+    fontFamily: 'Roboto',
+  }
+});
+
+export default theme;
