18b1d796f89149303bfb6d0718aa50f58da17040
richardscheid <rscheid@live.com>
2020-08-27 19:38:33 -0300

    refact: change return function

--- src/core/auth/auth.js
+++ src/core/auth/auth.js
@@ -1,7 +1,9 @@
 import { api } from '../api';
 
 const login = async ({ email = '', password = '' }) => {
-  return await api.post('login', { email, password });
+  const response = await api.post('login', { email, password });
+  const { data } = response;
+  return data;
 };
 
 export default { login };
