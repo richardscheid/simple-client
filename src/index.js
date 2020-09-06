0ec491dbe282a1ee08fac11e59b6bca496791f46
richardscheid <rscheid@live.com>
2020-09-06 14:50:22 -0300

    fix: import order

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import App from './App';
-import { store } from './redux';
 import { Provider } from 'react-redux';
+import { store } from './redux';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
