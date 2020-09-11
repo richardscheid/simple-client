e0e6f46c1eecd04c74f599813e454caadf1329f1
richardscheid <rscheid@live.com>
2020-09-11 17:51:07 -0300

    refact: validate token return

--- src/core/auth/validateToken/validateToken.js
+++ src/core/auth/validateToken/validateToken.js
@@ -1,16 +1,14 @@
 import { secure } from '../../api';
 
 const validateToken = async (token) => {
-  let response;
   const instance = secure(token);
 
   try {
-    response = await instance.post('/auth');
+    await instance.post('/auth');
   } catch {
     return false;
   }
-  console.log(response);
-  return response.data.auth;
+  return true;
 };
 
 export { validateToken };
