cd025d15ae50adecda2b931622cda3c82e732314
richardscheid <rscheid@live.com>
2020-09-27 19:21:26 -0300

    chore: create input style

--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -1,15 +1,32 @@
 import styled from 'styled-components';
 
-import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { withStyles, createStyles, fade } from '@material-ui/core/styles';
+
+import InputBase from '@material-ui/core/InputBase';
 
 export const TextFieldContainer = styled.div`
   margin-bottom: 12px;
 `;
 
-export const TextFieldStyles = makeStyles((theme) =>
+export const TextField = withStyles((theme) =>
   createStyles({
+    root: {
+      'label + &': {
+        marginTop: theme.spacing(3),
+      },
+    },
     input: {
+      borderRadius: 4,
+      position: 'relative',
       backgroundColor: theme.palette.common.white,
+      border: '1px solid #ced4da',
+      fontSize: 16,
+      padding: '10px 12px',
+      transition: theme.transitions.create(['border-color', 'box-shadow']),
+      '&:focus': {
+        boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+        borderColor: theme.palette.primary.main,
+      },
     },
   })
-);
+)(InputBase);
