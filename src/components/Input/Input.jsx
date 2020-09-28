efcbde87d5b0fa4f1a1d1a8af4bd490553c6a3db
richardscheid <rscheid@live.com>
2020-09-28 09:48:18 -0300

    chore: use input base

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,13 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import { TextFieldContainer, TextField } from './style';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+
+import { TextField } from './style';
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      marginBottom: theme.spacing(1),
+    },
+  })
+);
 
 const Input = (props) => {
+  const classes = useStyles();
+  const { label } = props;
   return (
-    <TextFieldContainer>
-      <TextField fullWidth autoComplete="off" {...props} />
-    </TextFieldContainer>
+    <FormControl fullWidth className={classes.root}>
+      <InputLabel shrink>{label}</InputLabel>
+
+      <TextField autoComplete="off" {...props} />
+    </FormControl>
   );
+};
+
+Input.propTypes = {
+  id: PropTypes.string,
+  type: PropTypes.string,
+  label: PropTypes.string,
+  helperText: PropTypes.string,
+  error: PropTypes.bool,
+  disabled: PropTypes.bool,
+  required: PropTypes.bool,
+};
+
+Input.defaultProps = {
+  id: null,
+  type: 'text',
+  label: null,
+  helperText: null,
+  error: false,
+  disabled: false,
+  required: false,
 };
 
 export { Input };
