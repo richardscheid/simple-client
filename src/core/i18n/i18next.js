510df8f86dfda6ae0098d9efc8bbd955eeb8173a
richardscheid <rscheid@live.com>
2020-09-28 15:46:50 -0300

    chore: use locales

--- src/core/i18n/i18next.js
+++ src/core/i18n/i18next.js
@@ -2,45 +2,20 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources: {
-      pt: {
-        translation: {
-          header: {
-            title: 'Simple',
-          },
-          login: {
-            signin: 'Entrar',
-          },
-          terms: {
-            email: 'E-mail',
-            password: 'Senha',
-            forgot: 'Esqueci a senha',
-          },
-        },
-      },
-      en: {
-        translation: {
-          header: {
-            title: 'Simple',
-          },
-          login: {
-            signin: 'Sign In',
-          },
-          terms: {
-            email: 'E-mail',
-            password: 'Password',
-            forgot: 'Forgot password',
-          },
-        },
-      },
-    },
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+import { pt, en, es } from './locales';
+
+const options = {
+  resources: {
+    pt: { translation: pt },
+    en: { translation: en },
+    es: { translation: es },
+  },
+  fallbackLng: 'en',
+  react: {
+    wait: true,
+  },
+};
+
+i18n.use(LanguageDetector).use(initReactI18next).init(options);
 
 export default i18n;
