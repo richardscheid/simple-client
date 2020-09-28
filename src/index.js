143ee4db454e69e777fc78f98a9d4ead8821b43a
richardscheid <rscheid@live.com>
2020-09-28 11:22:35 -0300

    chore: use i18n

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux';
+
+import './core/i18n';
 import App from './App';
 
 ReactDOM.render(
