3114fee494b818487fd42c7ce3f7e48e92fe646e
richardscheid <rscheid@live.com>
2021-02-05 08:13:55 -0300

    chore: add proptypes to blueprint

--- src/core/layout/blueprint/BluePrint.jsx
+++ src/core/layout/blueprint/BluePrint.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const BluePrint = props => {
+const BluePrint = (props) => {
   const { children } = props;
 
   return (
     <>
-      <div>
-        {/* Header */}
-      </div>
-
+      <div>Header</div>
     </>
-  )
-}
+  );
+};
+
+BluePrint.propTypes = {
+  children: PropTypes.node,
+};
+
+BluePrint.defaultProps = {
+  children: PropTypes.node,
+};
 
 export { BluePrint };
