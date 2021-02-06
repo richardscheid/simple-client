ca7ed0016d840135454ad205996f77b6b0ccef11
richardscheid <rscheid@live.com>
2021-02-06 16:43:19 -0300

    chore: add sidebar to layout

--- src/core/layout/Layout.jsx
+++ src/core/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Header } from './components';
+import { Header, Sidebar } from './components';
 
 const Layout = (props) => {
   const { children } = props;
@@ -11,7 +11,7 @@ const Layout = (props) => {
       <Header />
 
       <div>
-        Sidebar
+        <Sidebar />
         <div>
           <div>{children}</div>
 
