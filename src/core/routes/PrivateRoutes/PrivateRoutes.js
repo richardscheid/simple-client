4449074218f9961ad68a55b585f816aa353b9af3
richardscheid <rscheid@live.com>
2021-02-06 17:15:09 -0300

    chore: fix layout path

--- src/core/routes/PrivateRoutes/PrivateRoutes.js
+++ src/core/routes/PrivateRoutes/PrivateRoutes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Dashboard } from '../../../pages';
+import { Dashboard } from '@pages';
 import { Layout } from '../../layout';
 
 const PrivateRoutes = () => (
