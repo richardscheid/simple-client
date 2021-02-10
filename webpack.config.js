3935e5e78f649a58589aebd663c78a32c55df919
richardscheid <rscheid@live.com>
2021-02-10 17:19:02 -0300

    chore: create pages path

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ import path from 'path';
 module.exports = {
   resolve: {
     alias: {
-      rmv: path.resolve(__dirname, 'src/components/rmv/'),
+      pages: path.resolve(__dirname, 'src/pages/'),
     },
   },
 };
