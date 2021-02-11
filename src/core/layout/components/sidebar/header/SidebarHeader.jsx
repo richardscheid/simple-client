fc85ba041e38a27dae504d262f74e7af64fe7e1f
richardscheid <rscheid@live.com>
2021-02-11 17:23:18 -0300

    feat: use of styled icon button

--- src/core/layout/components/sidebar/header/SidebarHeader.jsx
+++ src/core/layout/components/sidebar/header/SidebarHeader.jsx
@@ -1,17 +1,18 @@
-import { Box, IconButton } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 import logo from '../../../../../assets/images/react.svg';
+import { StyledIconButton } from './style';
 
 const SidebarHeader = (props) => {
   return (
     <>
       <Box>
         <Link to="/dashboard">
-          <IconButton color="primary" size="small">
+          <StyledIconButton color="primary" size="small">
             <img src={logo} alt="Project logo" />
-          </IconButton>
+          </StyledIconButton>
         </Link>
       </Box>
     </>
