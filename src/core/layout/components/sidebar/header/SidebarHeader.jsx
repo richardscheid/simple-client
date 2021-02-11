23286d166c851b894a9e754ad108b54fa382c6cf
richardscheid <rscheid@live.com>
2021-02-11 08:17:43 -0300

    feat: add logo

--- src/core/layout/components/sidebar/header/SidebarHeader.jsx
+++ src/core/layout/components/sidebar/header/SidebarHeader.jsx
@@ -1,7 +1,21 @@
+import { Box, IconButton } from '@material-ui/core';
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../../../../../assets/images/react.svg';
 
 const SidebarHeader = (props) => {
-  return <></>;
+  return (
+    <>
+      <Box>
+        <Link to="/dashboard">
+          <IconButton color="primary" size="small">
+            <img src={logo} alt="Project logo" />
+          </IconButton>
+        </Link>
+      </Box>
+    </>
+  );
 };
 
 export { SidebarHeader };
