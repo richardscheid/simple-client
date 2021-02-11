468b9665ced5774e78b130280ec86812fa6c79d3
richardscheid <rscheid@live.com>
2021-02-11 08:17:55 -0300

    feat: add header to sidebar

--- src/core/layout/components/sidebar/Sidebar.jsx
+++ src/core/layout/components/sidebar/Sidebar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Drawer, Hidden, Paper } from '@material-ui/core';
+import { SidebarHeader } from './header';
 
 const Sidebar = (props) => {
   return (
     <>
-      <Hidden lgUp>
-        <Drawer anchor="left" variant="temporary" elevation={4}>
-          Sidebar Header
-        </Drawer>
-      </Hidden>
+      {/* <Hidden lgUp> */}
+      <Drawer open anchor="left" variant="permanent" elevation={16}>
+        <SidebarHeader />
+      </Drawer>
+      {/* </Hidden> */}
       <Hidden lgDown>
         <Paper>Sidebar Content</Paper>
       </Hidden>
