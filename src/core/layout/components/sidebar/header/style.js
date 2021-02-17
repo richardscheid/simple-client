788c83c63ceb9c2e8a6ab260200f359052fcd0e6
richardscheid <rscheid@live.com>
2021-02-17 11:44:24 -0300

    chore: styled sidebar header

--- src/core/layout/components/sidebar/header/style.js
+++ src/core/layout/components/sidebar/header/style.js
@@ -1,9 +1,19 @@
-import { withStyles } from '@material-ui/styles';
+import styled from 'styled-components';
 import { IconButton } from '@material-ui/core';
+import { withStyles } from '@material-ui/styles';
 
 export const StyledIconButton = withStyles({
   root: {
-    width: 32,
-    height: 32,
+    width: 16,
+    height: 16,
   },
 })(IconButton);
+
+export const StyledSidebarHeader = styled.div`
+  height: 32px;
+  width: 32px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+`;
