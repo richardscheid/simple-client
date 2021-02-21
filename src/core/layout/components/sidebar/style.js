f387f01bed221e3ec4879b0061aad30c28a3e5bd
richardscheid <rscheid@live.com>
2021-02-21 16:54:29 -0300

    chore: paper style

--- src/core/layout/components/sidebar/style.js
+++ src/core/layout/components/sidebar/style.js
@@ -1,5 +1,6 @@
 import { withStyles } from '@material-ui/styles';
-import { Drawer } from '@material-ui/core';
+import { Drawer, Paper } from '@material-ui/core';
+import styled from 'styled-components';
 
 export const SidebarWrapper = withStyles({
   root: {
@@ -11,3 +12,14 @@ export const SidebarWrapper = withStyles({
     flexDirection: 'column',
   },
 })(Drawer);
+
+export const SidebarPaper = styled(Paper)`
+  display: 'flex';
+  flex-direction: 'column';
+  min-height: '100%';
+  overflow: 'hidden';
+  z-index: 60;
+  width: 270;
+  transition: 'all 0.5s cubic-bezier(0.685, 0.0473, 0.346, 1)';
+  position: ${(props) => (props.fixed ? 'static' : 'fixed')};
+`;
