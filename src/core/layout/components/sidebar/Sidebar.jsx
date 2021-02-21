d0f6bfa642c35b0099743a80aa51cf7b18329d82
richardscheid <rscheid@live.com>
2021-02-21 17:16:51 -0300

    fix: types default

--- src/core/layout/components/sidebar/Sidebar.jsx
+++ src/core/layout/components/sidebar/Sidebar.jsx
@@ -1,25 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Hidden } from '@material-ui/core';
 import { SidebarWrapper, SidebarPaper } from './style';
 import { SidebarHeader } from './header';
 
 const Sidebar = (props) => {
-  const { sidebarFixed, sidebarShadow } = props;
+  const { sidebarFixed, sidebarShadow, sidebarToggleMobile } = props;
+
+  const closeDrawer = () => !sidebarToggleMobile;
 
   return (
     <>
       {/* <Hidden lgUp> */}
-      <SidebarWrapper open anchor="left" variant="permanent" elevation={16}>
+      <SidebarWrapper
+        open={sidebarToggleMobile}
+        anchor="left"
+        variant="temporary"
+        elevation={4}
+        onClose={closeDrawer}
+      >
         <SidebarHeader />
       </SidebarWrapper>
       {/* </Hidden> */}
-      <Hidden lgDown>
-        <SidebarPaper square elevation={sidebarShadow ? 11 : 3}>
-          <SidebarHeader fixed={sidebarFixed} />
-        </SidebarPaper>
-      </Hidden>
+      {/* <Hidden lgDown> */}
+      <SidebarPaper square elevation={sidebarShadow ? 11 : 3}>
+        <SidebarHeader fixed={sidebarFixed} />
+      </SidebarPaper>
+      {/* </Hidden> */}
     </>
   );
+};
+
+Sidebar.propTypes = {
+  sidebarFixed: PropTypes.bool,
+  sidebarShadow: PropTypes.bool,
+  sidebarToggleMobile: PropTypes.bool,
+};
+
+Sidebar.defaultProps = {
+  sidebarFixed: true,
+  sidebarShadow: false,
+  sidebarToggleMobile: false,
 };
 
 export { Sidebar };
