831eceab73c0c79a06626a14e4254e4ec0741b25
richardscheid <rscheid@live.com>
2021-02-21 17:40:54 -0300

    chore: props values

--- src/core/layout/components/header/Header.jsx
+++ src/core/layout/components/header/Header.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import { AppBar, Box, Hidden } from '@material-ui/core';
 
 const Header = (props) => {
+  const toggleSidebarMobile = () =>
+    setSidebarToggleMobile(!sidebarToggleMobile);
+
+  const { headerShadow, headerFixed, sidebarToggleMobile } = props;
+
   return (
     <>
-      <AppBar>
+      <AppBar
+        color="secondary"
+        position={headerFixed ? 'fixed' : 'absolute'}
+        elevation={headerShadow ? 11 : 3}
+      >
         <Box>
           <Hidden>
             <Box>Header</Box>
