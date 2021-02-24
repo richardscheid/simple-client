d14bb5817e102ec60bbe23da2ffeacaee9c14f5c
richardscheid <rscheid@live.com>
2021-02-23 21:48:01 -0300

    chore: create box logo component

---
+++ src/components/Box/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { Box as CoreBox } from '@material-ui/core';
+
+export const BoxLogo = styled(CoreBox)`
+  display: flex;
+  align-items: center;
+`;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { Input } from './Input';
 export { Button } from './Button';
 export { LayoutContent } from './Layout/LayoutContent';
+
+export { BoxLogo } from './Box/style';
