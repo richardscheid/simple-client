359df7f00f5a57ab51965b1c664bfce8d3d279d0
richardscheid <rscheid@live.com>
2021-02-23 21:41:26 -0300

    chore: create box

--- src/core/layout/components/header/style.js
+++ src/core/layout/components/header/style.js
@@ -1,8 +1,21 @@
-import { AppBar as CoreAppBar, Box } from '@material-ui/core';
 import styled from 'styled-components';
+import { AppBar as CoreAppBar, Box as CoreBox } from '@material-ui/core';
 
 export const AppBar = styled(CoreAppBar)`
   height: var(--header-height);
   display: flex;
   flex-direction: row;
+`;
+
+export const Box = styled(CoreBox)`
+  height: 100%;
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+  padding-left: var(--spacer);
+  padding-right: var(--spacer);
+
+  .btn-inverse:hover {
+    background: rgba(255, 255, 255, 0.07);
+  }
 `;
