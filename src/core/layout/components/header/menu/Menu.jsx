8fc89048e891aee51e1ababecc920478303a2182
richardscheid <rscheid@live.com>
2021-02-26 18:21:08 -0300

    chore: create avatar

--- src/core/layout/components/header/menu/Menu.jsx
+++ src/core/layout/components/header/menu/Menu.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import { Avatar, Box } from '@material-ui/core';
 import { Button } from './style';
+
+import avatar from '../../../../../assets/images/avatar.png';
 
 const Menu = (props) => {
   return (
     <>
-      <Button color="inherit" />
+      <Button color="inherit">
+        <Box>
+          <Avatar sizes={44} src={avatar} alt="Avatar" />
+        </Box>
+      </Button>
     </>
   );
 };
