2b65db570f380193e4685d12c347d91a705658ce
richardscheid <rscheid@live.com>
2021-03-03 14:32:06 -0300

    chore: header title

--- src/core/layout/components/header/Header.jsx
+++ src/core/layout/components/header/Header.jsx
@@ -37,7 +37,7 @@ const Header = (props) => {
                 </IconButtonHeader>
               </LinkHeader>
               <Hidden smDown>
-                <BoxText>Simple Corp</BoxText>
+                <BoxText>Simple Header</BoxText>
               </Hidden>
             </BoxLogo>
           </Hidden>
