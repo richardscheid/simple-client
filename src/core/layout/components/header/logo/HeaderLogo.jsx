a3658ae610daee23321f0930ca51857992da0687
richardscheid <rscheid@live.com>
2021-03-03 14:12:54 -0300

    chore: create logo for header

---
+++ src/core/layout/components/header/logo/HeaderLogo.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import logo from '../../../../../assets/images/react.svg';
+import { LogoHeaderContent, IconButton, BoxTitle, Image, Link } from './style';
+import { BoxLogo } from '../../../../../components';
+
+const HeaderLogo = () => {
+  return (
+    <>
+      <LogoHeaderContent>
+        <BoxLogo>
+          <Link to="/dashboard">
+            <IconButton color="primary" size="small">
+              <Image src={logo} alt="Project logo" />
+            </IconButton>
+          </Link>
+          <BoxTitle>Simple</BoxTitle>
+        </BoxLogo>
+      </LogoHeaderContent>
+    </>
+  );
+};
+
+export { HeaderLogo };
