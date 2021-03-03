c5ac3688f772e5df55f675629394536036f44988
richardscheid <rscheid@live.com>
2021-03-03 16:26:30 -0300

    chore: create layout styles

--- src/core/layout/style.js
+++ src/core/layout/style.js
@@ -11,3 +11,23 @@ export const LayoutWrapper = styled.div`
   position: relative;
   min-height: 100vh;
 `;
+
+export const LayoutContent = styled.div`
+  flex: 1;
+  display: flex;
+  padding: 0;
+  flex-direction: column;
+  transition: all 0.5s var(--app-sidebar-transition);
+  padding-top: var(--header-height);
+`;
+
+export const InnerContent = styled(LayoutContent)`
+  padding: var(--layout-spacer) var(--layout-spacer) (var(--layout-spacer) / 2);
+  display: flex;
+  flex: 1;
+`;
+
+export const WrapperContent = styled(InnerContent)`
+  display: block;
+  width: 100%;
+`;
