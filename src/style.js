9505d366ccd877c245303a0193ab1592c5b2f504
richardscheid <rscheid@live.com>
2021-03-03 15:42:13 -0300

    chore: create global transition style

--- src/style.js
+++ src/style.js
@@ -26,5 +26,6 @@ export const GlobalStyles = createGlobalStyle`
     --font-size-base: 1rem;
     --font-size-lg: 1rem * 1.25;
     --font-size-sm: 1rem * .875;
+    --app-sidebar-transition: cubic-bezier(0.685, 0.0473, 0.346, 1);
   }
 `;
