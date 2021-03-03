4c683b988ed2e5e93781c22039824858774a67e9
richardscheid <rscheid@live.com>
2021-03-03 15:24:44 -0300

    chore: fix styles

--- src/core/layout/components/sidebar/style.js
+++ src/core/layout/components/sidebar/style.js
@@ -11,12 +11,12 @@ export const Drawer = styled(CoreDrawer)`
 `;
 
 export const Paper = styled(CorePaper)`
+  width: var(--sidebar-width);
+  min-height: 100%;
   display: flex;
   flex-direction: column;
-  min-height: 100%;
   overflow: hidden;
   z-index: 60;
-  width: 270;
   transition: all 0.5s cubic-bezier(0.685, 0.0473, 0.346, 1);
   position: ${(props) => (props.fixed ? 'static' : 'fixed')};
 `;
