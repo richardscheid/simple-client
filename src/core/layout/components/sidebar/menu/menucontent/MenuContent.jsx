679d9fb0488ef35a849d7759bc1a6746ba4ffb91
richardscheid <rscheid@live.com>
2021-03-06 11:57:03 -0300

    chore: create menu content

---
+++ src/core/layout/components/sidebar/menu/menucontent/MenuContent.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
+
+import { Typography } from './style';
+import { MenuList } from '../menulist';
+
+const MenuContent = (props) => {
+  const { title, pages, ...rest } = props;
+  const { location } = useLocation();
+
+  return (
+    <div {...rest}>
+      {title && (
+        <Typography className="app-sidebar-heading">{title}</Typography>
+      )}
+      <MenuList depth={0} pages={pages} location={location} />
+    </div>
+  );
+};
+
+MenuContent.propTypes = {
+  pages: PropTypes.array.isRequired,
+  title: PropTypes.string,
+};
+
+MenuContent.defaultProps = {
+  title: PropTypes.string,
+};
+
+export { MenuContent };
