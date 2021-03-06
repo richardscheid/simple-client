ca57e257d65e8522a6819e744952719c1c657368
richardscheid <rscheid@live.com>
2021-03-06 10:05:35 -0300

    chore: i18n menu data

--- src/core/layout/components/sidebar/menu/data/index.js
+++ src/core/layout/components/sidebar/menu/data/index.js
@@ -8,8 +8,7 @@ export default [
               "enable":"true",
               "content":[
                   {
-                      "label":"Dashboard",
-                      "description":"description",
+                      "label":"menu.dashboard.title",
                       "to":"/dashboard",
                       "enable":"true",
                       "role":"CUSTOMER"
@@ -17,23 +16,23 @@ export default [
               ]
           },
           {
-              "title":"Cadastros",
+              "title":"menu.registers",
               "enable":"true",
               "content":[
                   {
-                      "title":"Usuários",
-                      "description":"description",
+                      "title":"menu.user.title",
+                      "description":"menu.user.description",
                       "to":"/users",
                       "enable":"true",
                       "role":"CUSTOMER"
                   },
                   {
-                      "title":"Categorias",
-                      "description":"description",
+                      "title":"menu.category.title",
+                      "description":"menu.category.description",
                       "to":"/categories",
                       "enable":"true",
                       "role":"CUSTOMER"
-                  },
+                  }
               ]
           }
       ]`
