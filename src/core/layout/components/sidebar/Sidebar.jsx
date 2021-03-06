873910cc6f0049a7bc028018c70c2d83bf961dd0
richardscheid <rscheid@live.com>
2021-03-06 11:58:59 -0300

    chore: use of menu

--- src/core/layout/components/sidebar/Sidebar.jsx
+++ src/core/layout/components/sidebar/Sidebar.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+
 import { Hidden } from '@material-ui/core';
 import { Drawer, Paper } from './style';
 import { SidebarHeader } from './header';
+import { MenuContainer } from './menu/menucontainer';
 
 const Sidebar = (props) => {
   const { sidebarFixed, sidebarShadow, sidebarToggleMobile } = props;
@@ -20,11 +23,15 @@ const Sidebar = (props) => {
           onClose={closeDrawer}
         >
           <SidebarHeader />
+          <PerfectScrollbar>{MenuContainer}</PerfectScrollbar>
         </Drawer>
       </Hidden>
       <Hidden mdDown>
         <Paper square elevation={sidebarShadow ? 11 : 3}>
           <SidebarHeader fixed={sidebarFixed} />
+          <PerfectScrollbar options={{ wheelPropagation: false }}>
+            {MenuContainer}
+          </PerfectScrollbar>
         </Paper>
       </Hidden>
     </>
