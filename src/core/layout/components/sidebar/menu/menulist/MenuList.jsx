9ccc70531b8deb9ddb19c435c5be526a1d499691
richardscheid <rscheid@live.com>
2021-03-06 10:26:17 -0300

    chore: create menu list

---
+++ src/core/layout/components/sidebar/menu/menulist/MenuList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { MenuData } from '../menudata';
+import { List } from './style';
+
+const MenuList = (props) => {
+  const { pages, ...rest } = props;
+
+  return (
+    <List>
+      {pages.reduce((items, page) => MenuData({ items, page, ...rest }), [])}
+    </List>
+  );
+};
+
+MenuList.propTypes = {
+  depth: PropTypes.number,
+  pages: PropTypes.array,
+};
+
+MenuList.defaultProps = {
+  depth: PropTypes.number,
+  pages: PropTypes.array,
+};
+
+export { MenuList };
