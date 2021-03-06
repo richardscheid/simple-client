f3e3d5f4dd160d4e0a202e674a2b67201fbfe130
richardscheid <rscheid@live.com>
2021-03-06 11:57:22 -0300

    chore: create menu content style

---
+++ src/core/layout/components/sidebar/menu/menucontent/style.js
@@ -0,0 +1,12 @@
+import { Typography as CoreTypography } from '@material-ui/core';
+import styled from 'styled-components';
+
+export const Typography = styled(CoreTypography)`
+  padding: var(--spacer) var(--spacer) (var(--spacer) / 1.1);
+  font-weight: bold;
+  color: $blue;
+  text-transform: uppercase;
+  font-size: $font-size-sm;
+  white-space: nowrap;
+  transition: all 0.5s cubic-bezier(0.685, 0.0473, 0.346, 1);
+`;
