3e2f81ef9be8d4cab1225b876c0b7e8b3464709e
richardscheid <rscheid@live.com>
2021-03-07 16:56:28 -0300

    chore: add layout to private routes

--- src/core/routes/PrivateRoutes/PrivateRoutes.js
+++ src/core/routes/PrivateRoutes/PrivateRoutes.js
@@ -1,18 +1,63 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { Dashboard } from '../../../pages';
+import React, { Suspense } from 'react';
+import { Switch, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 import { Layout } from '../../layout';
 
-const PrivateRoutes = () => (
-  <Switch>
-    <Layout>
-      <Switch>
-        <Route path="/dashboard">
-          <Dashboard />
+import { Dashboard } from '../../../pages';
+
+const PrivateRoutes = () => {
+  const location = useLocation();
+
+  const pageVariants = {
+    initial: {
+      opacity: 0,
+      scale: 0.99,
+    },
+    in: {
+      opacity: 1,
+      scale: 1,
+    },
+    out: {
+      opacity: 0,
+      scale: 1.01,
+    },
+  };
+
+  const pageTransition = {
+    type: 'tween',
+    ease: 'anticipate',
+    duration: 0.4,
+  };
+
+  return (
+    <AnimatePresence>
+      <Suspense
+        fallback={
+          <div className="w-50 mx-auto">
+            Please wait while we load the live preview examples
+          </div>
+        }
+      >
+        <Route path={['/dashboard', '/users', '/categories']}>
+          <Layout>
+            <Switch location={location} key={location.pathname}>
+              <motion.div
+                transition={pageTransition}
+                variants={pageVariants}
+                initial="initial"
+                animate="in"
+                exit="out"
+              >
+                <Route path="/dashboard">
+                  <Dashboard />
+                </Route>
+              </motion.div>
+            </Switch>
+          </Layout>
         </Route>
-      </Switch>
-    </Layout>
-  </Switch>
-);
+      </Suspense>
+    </AnimatePresence>
+  );
+};
 
 export { PrivateRoutes };
