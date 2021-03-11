8308b306cc684b4b90084f39fb54b704a727ca14
richardscheid <rscheid@live.com>
2021-03-11 18:52:28 -0300

    chore: add selected button

--- src/core/layout/components/sidebar/menu/menuitem/MenuItem.jsx
+++ src/core/layout/components/sidebar/menu/menuitem/MenuItem.jsx
@@ -35,11 +35,7 @@ const MenuItem = (props) => {
   if (children) {
     return (
       <ListItem {...rest} disableGutters>
-        <MenuButton
-          // active={open}
-          depth={depth}
-          onClick={handleToggle}
-        >
+        <MenuButton active={open} depth={depth} onClick={handleToggle}>
           <span>{t(title)}</span>
           {open ? (
             <ExpandLess color="inherit" />
