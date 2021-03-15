778067f3b6985a026a1945f037e225c396a9be02
richardscheid <rscheid@live.com>
2021-03-15 17:20:36 -0300

    chore: create paper container

---
+++ src/core/layout/components/page/title/style.js
@@ -0,0 +1,18 @@
+import { Paper as CorePaper } from '@material-ui/core';
+import styled from 'styled-components';
+
+export const PaperContainer = styled(CorePaper)`
+  margin: 14px 14px 14px;
+  padding: 14px 42px 14px 14px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: rgba(#ffff, 0.5);
+  transition: var(--transition-base);
+  position: relative;
+
+  @include media-breakpoint-down(md) {
+    flex-direction: column;
+    padding-right: 14px;
+  }
+`;
