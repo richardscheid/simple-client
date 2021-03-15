f7f62bcbe550de0c2060889c9692916fab4be53b
richardscheid <rscheid@live.com>
2021-03-15 17:33:02 -0300

    chore: create title container

--- src/core/layout/components/page/title/TitleContainer.jsx
+++ src/core/layout/components/page/title/TitleContainer.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const TitleContainer = (props) => {};
+import { PaperContainer, PaperDescription, PaperHeading, Title } from './style';
+
+const TitleContainer = (props) => {
+  const { title, description } = props;
+  return (
+    <>
+      <PaperContainer>
+        <PaperHeading>
+          <Title>{title}</Title>
+          <PaperDescription>{description}</PaperDescription>
+        </PaperHeading>
+      </PaperContainer>
+    </>
+  );
+};
+
+TitleContainer.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 
 export { TitleContainer };
