27a441c803cea41ffc0dcc9b4bb65ef88dcae0a3
richardscheid <rscheid@live.com>
2021-03-16 18:40:38 -0300

    chore: create user page

---
+++ src/pages/user/User.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { TitleContainer, PageContainer } from '../../core/layout/components';
+
+const User = (props) => {
+  return (
+    <>
+      <TitleContainer title="Usuários" />
+
+      <PageContainer />
+    </>
+  );
+};
+
+export { User };
