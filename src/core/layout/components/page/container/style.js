88a0e8658fe40a12a685519c31f7b2ac2a629e1b
richardscheid <rscheid@live.com>
2021-03-16 17:45:19 -0300

    chore: create card content style

---
+++ src/core/layout/components/page/container/style.js
@@ -0,0 +1,9 @@
+import {
+  Card as CoreCard,
+  CardContent as CoreContent,
+} from '@material-ui/core';
+import styled from 'styled-components';
+
+export const CardContent = styled(CoreContent)`
+  padding: 3px;
+`;
