78f3fb3864a8cc3f9618af35ddb607ad50373f3f
richardscheid <rscheid@live.com>
2021-03-25 20:49:36 -0300

    refact: title responsiviness

--- src/core/layout/components/page/title/style.js
+++ src/core/layout/components/page/title/style.js
@@ -2,26 +2,10 @@ import { Paper as CorePaper } from '@material-ui/core';
 import styled from 'styled-components';
 
 export const PaperContainer = styled(CorePaper)`
-  margin: 14px 14px 14px;
-  padding: 14px 42px 14px 14px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background: rgba(#ffff, 0.5);
-  transition: var(--transition-base);
+  padding: 10px 10px 10px 20px;
   position: relative;
 
-  @include media-breakpoint-down(md) {
-    flex-direction: column;
-    padding-right: 14px;
-  }
-`;
-
-export const PaperHeading = styled(PaperContainer)`
-  padding-right: (14px);
-
-  @include media-breakpoint-down(md) {
-    padding-right: 0;
+  @media screen and (max-width: 800px) {
     text-align: center;
   }
 `;
