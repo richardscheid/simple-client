af1f27aa9e021c3dec94d58efd0979be72c2c614
richardscheid <rscheid@live.com>
2021-03-25 20:20:36 -0300

    chore: add use transaction

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { TitleContainer, PageContainer } from '../../core/layout/components';
 
 const User = (props) => {
+  const { t } = useTranslation();
+
   return (
     <>
-      <TitleContainer title="Usuários" />
+      <TitleContainer title={t('menu.user.title')} />
 
       <PageContainer />
     </>
