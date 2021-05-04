59e2c31264b4e2a18fc6da402e57d248e90bb36e
richardscheid <rscheid@live.com>
2021-05-04 08:56:54 -0300

    create request data

--- src/core/http/request/HttpRequest.js
+++ src/core/http/request/HttpRequest.js
@@ -1,13 +1,25 @@
 import { useSelector } from 'react-redux';
 
+class RequestData {
+  constructor(url, method, body, headers) {
+    this.url = url;
+    this.method = method;
+    this.body = body;
+    this.headers = headers;
+  }
+}
+
 const HttpRequest = ({ data }) => {
   const token = useSelector((state) => state.auth.token);
 
-  data.headers = [{ Accept: 'application/json' }];
+  const request = new RequestData();
+  request.url = data.url;
+  request.method = data.method;
+  request.body = data.body;
+  request.headers = [{ Accept: 'application/json' }];
+  if (token) request.headers.push({ Authorization: `Bearer ${token}` });
 
-  if (token) data.headers.push({ Authorization: `Bearer ${token}` });
-
-  return data;
+  return request;
 };
 
 export default HttpRequest;
